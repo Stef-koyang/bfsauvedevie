3247933ca2d571c3787ee851993b50ee69fde97d
Stef-koyang <101928146+Stef-koyang@users.noreply.github.com>
2025-07-26 22:50:37 +0100

    Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between bg-blue-900 text-white p-4">
       <img
-        src="/beni_futur_logo.png"
+        src="/public/beni_futur_logo.png"
         alt="Beni Futur"
         className="h-12 object-contain"
       />
       <h1 className="text-2xl font-bold tracking-wide">BF SAUVE DES VIES</h1>
-      <img src="/rdc_logo.png" alt="RDC" className="h-12 object-contain" />
+      <img src="/public/alert.mp3" alt="RDC" className="h-12 object-contain" />
     </header>
   );
 }
